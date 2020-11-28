a68e46619e2633846a5a9e390499d704434bfa90
mariobalrod <marioballestero.rdg@gmail.com>
2020-11-28 12:39:00 +0100

    estructura router inicial

--- ejercicios/ejercicios-1t/entrega/src/containers/Main.jsx
+++ ejercicios/ejercicios-1t/entrega/src/containers/Main.jsx
@@ -1,13 +1,33 @@
 import styled from 'styled-components';
+import { Link as DefaultLink } from 'react-router-dom'
 
 const Container = styled.div`
+  width: 1000px;
+  margin: 450px auto;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
 
+const Link = styled(DefaultLink)`
+    width: 260px;
+    color:  #ccc;
+    font-size: 30px;
+    font-weight: 700;
+    text-align: center;
+    text-decoration: none;
+
+    :hover {
+        color: #cca;
+    }
 `;
 
 const Main = () => {
     return (
         <Container>
-            <h1>Main</h1>
+            <Link to="/among">Among Us</Link>
+            <Link to="/tres">Tres en raya</Link>
+            <Link to="/groups">Creación Grupos</Link>
         </Container>
     )
 }
